00dd372a3bb79681c430ea534870c445e78c185f
aarhusdavid <aarhus@chapman.edu>
2019-09-23 03:15:21 +0000

    updated readme

--- GOL_main.cpp
+++ GOL_main.cpp
@@ -6,9 +6,14 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    string map;
     Readerfile r;
     int start = 0;
+    int rows = 0;
+    int columns = 0;
+    double pop = 0.0;
+    int gameMode = 0;
+    int results = 0;
+
     cout << "Would you like to import a file (1) or use a randomized file (2)?" << endl;
     cin >> start;
     while(start != 1 && start != 2)
@@ -26,18 +31,29 @@ int main(int argc, char** argv)
 
         else if (start == 2)
         {
-            double pop = 0.0;
-            int rows = 0;
-            int columns = 0;
             cout << "How many rows would you like to have?" << endl;
             cin >> rows;
             cout << "How many columns would you like to have?" << endl;
             cin >> columns;
             cout << "Now enter a decimal value greater than 0 and less than or equal to 1.\nThis will represent the initial population density of the world" << endl;
             cin >> pop;
+            cout << "What Game Mode would you like to play?\nClassic (1)\nDounut (2)\nMirror (3)" << endl;
+            cin >> gameMode;
+            cout << "Would you like a pause between every generations, allowing you to see each generation or would you like the generations to be printed to a file?\nPause (1)\nPrint results to Output file (2)" << endl;
+            cin >> results;
+        }
 
-
-
+        if (gameMode == 1)
+        {
+            return 0;
+        }
+        else if (gameMode == 2)
+        {
+            return 0;
+        }
+        else if (gameMode == 3)
+        {
+            return 0;
         }
 
     return 0;
---
+++ Readerfile.h
@@ -0,0 +1,16 @@
+#include <iostream> //preprocessor directive
+
+using namespace std; //allows you to print
+
+class Readerfile
+{
+public:
+    Readerfile(); //constructor
+    ~Readerfile(); //destructor
+
+    //initialized method
+    string read(string input);
+
+
+
+};
